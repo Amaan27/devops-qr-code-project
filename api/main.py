08a34b8a51b835ce6d206ea4dfb5ba0ff4ea2e21
amaan <otheramaan@gmail.com>
2025-09-09 22:57:27 +0530

    Change in main.py

--- api/main.py
+++ api/main.py
@@ -29,7 +29,7 @@ s3 = boto3.client(
     aws_access_key_id= os.getenv("AWS_ACCESS_KEY"),
     aws_secret_access_key= os.getenv("AWS_SECRET_KEY"))
 
-bucket_name = 'YOUR_BUCKET_NAME' # Add your bucket name here
+bucket_name = 'qr-storage-bucket' # Add your bucket name here
 
 @app.post("/generate-qr/")
 async def generate_qr(url: str):
@@ -55,11 +55,22 @@ async def generate_qr(url: str):
 
     try:
         # Upload to S3
-        s3.put_object(Bucket=bucket_name, Key=file_name, Body=img_byte_arr, ContentType='image/png', ACL='public-read')
+        s3.put_object(Bucket=bucket_name, Key=file_name, Body=img_byte_arr, ContentType='image/png')
         
         # Generate the S3 URL
-        s3_url = f"https://{bucket_name}.s3.amazonaws.com/{file_name}"
-        return {"qr_code_url": s3_url}
+        # s3_url = f"https://{bucket_name}.s3.amazonaws.com/{file_name}"
+        # return {"qr_code_url": s3_url}
+        s3_url = s3.generate_presigned_url(
+    'get_object',
+    Params={'Bucket': bucket_name, 'Key': file_name},
+    ExpiresIn=3600  # valid for 1 hour
+)
+
+    # except Exception as e:
+    #     raise HTTPException(status_code=500, detail=str(e))
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+      print("ERROR:", e)  # <-- add this
+      raise HTTPException(status_code=500, detail=str(e))
+    
+    return {"qr_code_url": s3_url}
     
